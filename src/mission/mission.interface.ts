4b987a3f54fc30572a1c8e6752dfcef67c2f7b80
DarkyMCPJ <harumi.ploy@gmail.com>
2026-03-02 18:37:33 +0700

    pRETTYIER??

--- src/mission/mission.interface.ts
+++ src/mission/mission.interface.ts
@@ -1,11 +1,10 @@
 export interface IMission {
-
-id: string;
-codename: string;
-status: string;
-targetName: string;
-riskLevel: string;
-startDate: string;
-endDate: string;
-
+  id: string;
+  codename: string;
+  status: string;
+  targetName: string;
+  riskLevel: string;
+  startDate: string;
+  endDate: string | null;
+  durationDays?: number;
 }
--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -1,33 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MissionService {
-private readonly mission = [
-{ id: 1, codename: "OPERATION_STORM", status: "ACTIVE" },
-{ id: 2, codename: "SILENT_SNAKE", status: "COMPLETED" },
-{ id: 3, codename: "RED_DAWN", status: "FAILED" },
-{ id: 4, codename: "BLACKOUT", status: "ACTIVE" },
-{ id: 5, codename: "ECHO_FALLS", status: "COMPLETED" },
-{ id: 6, codename: "GHOST_RIDER", status: "COMPLETED" },
+  private readonly mission = [
+    { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
+    { id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
+    { id: 3, codename: 'RED_DAWN', status: 'FAILED' },
+    { id: 4, codename: 'BLACKOUT', status: 'ACTIVE' },
+    { id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
+    { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' },
   ];
 
-  
-getSummary() {
-const res = {
-ACTIVE: 0,
-COMPLETED: 0,
-FAILED: 0,
+  getSummary() {
+    const res = {
+      ACTIVE: 0,
+      COMPLETED: 0,
+      FAILED: 0,
     };
-    
-for (let i = 0; i < this.mission.length; i++) {
 
-const data = this.mission[i];
-if (data.status in res) {
-res[data.status as keyof typeof res]++;
+    for (let i = 0; i < this.mission.length; i++) {
+      const data = this.mission[i];
+      if (data.status in res) {
+        res[data.status as keyof typeof res]++;
+      }
+    }
 
-}
-}
-
-return res;
-}
+    return res;
+  }
 }

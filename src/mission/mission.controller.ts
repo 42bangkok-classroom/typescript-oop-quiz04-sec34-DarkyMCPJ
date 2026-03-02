86ab4251d084a7fa835c4049d639e8149e9d33d4
DarkyMCPJ <harumi.ploy@gmail.com>
2026-03-02 19:46:55 +0700

    tired

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,54 +1,26 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Query,
-  Delete,
-} from "@nestjs/common";
-import { MissionService } from "./mission.service";
-
-@Controller("missions")
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
+import { MissionService } from './mission.service';
+@Controller('missions')
 export class MissionController {
   constructor(private readonly missionService: MissionService) {}
 
   @Get()
-  getMissions() {
+  findAll() {
     return this.missionService.findAll();
   }
 
-  @Get("summary")
+  @Get('summary')
   getSummary() {
     return this.missionService.getSummary();
   }
 
-  @Get(":id")
-  getMissionById(
-    @Param("id") id: string,
-    @Query("clearance") clearance?: string,
-  ) {
+  @Get(':id')
+  findOne(@Param('id') id: string, @Query('clearance') clearance: string) {
     return this.missionService.findOne(id, clearance);
   }
-
-  @Delete(":id")
-  deleteMission(@Param("id") id: string) {
-    return this.missionService.remove(id);
-  }
-
+  
   @Post()
-  createMission(
-    @Body()
-    mission: {
-      codename: string;
-      targetName: string;
-      riskLevel: string;
-      startDate: string;
-      id: string;
-      status: string;
-      endDate: string;
-    },
-  ) {
-    return this.missionService.create(mission);
+  create(@Body() body: any) {
+    return this.missionService.create(body);
   }
 }

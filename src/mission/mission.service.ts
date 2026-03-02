fa759cfaa8e653a471b6b7b557a8279c4b3cf39c
DarkyMCPJ <harumi.ploy@gmail.com>
2026-03-02 18:30:56 +0700

    p01-4

--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -1,21 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class MissionService {
-private readonly missions = [
-{ id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
-{ id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
-{ id: 3, codename: 'RED_DAWN', status: 'FAILED' },
-{ id: 4, codename: 'BLACKOUT', status: 'ACTIVE' },
-{ id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
-{ id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' },
-];
+private readonly mission = [
+{ id: 1, codename: "OPERATION_STORM", status: "ACTIVE" },
+{ id: 2, codename: "SILENT_SNAKE", status: "COMPLETED" },
+{ id: 3, codename: "RED_DAWN", status: "FAILED" },
+{ id: 4, codename: "BLACKOUT", status: "ACTIVE" },
+{ id: 5, codename: "ECHO_FALLS", status: "COMPLETED" },
+{ id: 6, codename: "GHOST_RIDER", status: "COMPLETED" },
+  ];
 
-getSummary(): Record<string, number> {
-return this.missions.reduce((acc: Record<string, number>, mission) => {
-acc[mission.status] = (acc[mission.status] || 0) + 1;
-return acc;
+  
+getSummary() {
+const res = {
+ACTIVE: 0,
+COMPLETED: 0,
+FAILED: 0,
+    };
+    
+for (let i = 0; i < this.mission.length; i++) {
 
-},{});
+const data = this.mission[i];
+if (data.status in res) {
+res[data.status as keyof typeof res]++;
+
+}
+}
+
+return res;
 }
 }
